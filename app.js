a5580febb3fc1d671c3321b20d0090c4bae2dea6
Rory Demers <roryjackd@yahoo.com>
2019-05-30 14:34:23 -0600

    landing and campgrounds page added

--- app.js
+++ app.js
@@ -2,6 +2,18 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.set("view engine", "ejs");
+
+app.get('/', (req, res) => res.render('landing'))
+
+app.get('/campgrounds', function(req, res){
+    let campgrounds = [
+        {name: 'Salmon Creek', image: 'https://images.freeimages.com/images/large-previews/e4c/camping-tent-1058140.jpg'},
+        {name: 'Granite Hill', image: 'https://images.freeimages.com/images/large-previews/a25/empty-campground-1442093.jpg'},
+        {name: 'Mountains Rest', image: 'https://images.freeimages.com/images/large-previews/fc3/farmington-river-1346136.jpg'}
+    ]
+
+    res.render("campgrounds", {campgrounds:campgrounds});
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))

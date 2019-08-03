6ffcc7e470417369300d731eb96b49a85dcb29da
Rory Demers <roryjackd@yahoo.com>
2019-08-03 14:41:21 -0600

    Logout added

--- app.js
+++ app.js
@@ -70,7 +70,7 @@ app.get("/campgrounds/:id", function(req, res){
     req.params.id
 });
 
-app.get("/campgrounds/:id/comments/new", function(req, res){
+app.get("/campgrounds/:id/comments/new", isLoggedIn, function(req, res){
     
     Campground.findById(req.params.id, function(err, campground){
         if(err){
@@ -81,7 +81,7 @@ app.get("/campgrounds/:id/comments/new", function(req, res){
     })
 });
 
-app.post("/campgrounds/:id/comments", function(req, res){
+app.post("/campgrounds/:id/comments",isLoggedIn, function(req, res){
     Campground.findById(req.params.id, function(err, campground){
         if(err){
             console.log(err);
@@ -129,5 +129,18 @@ app.post("/login", passport.authenticate("local",
         failureRedirect: "/login"
     }), function(req, res){
 });
+
+//LOGOUT
+app.get("/logout", function(req, res){
+    req.logout();
+    res.redirect("/campgrounds");
+})
+
+function isLoggedIn(req, res, next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+    res.redirect("/login");
+}
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))

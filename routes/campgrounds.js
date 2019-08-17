b20c5ba39beff6bb779119475d2c864a342b4986
Rory Demers <roryjackd@yahoo.com>
2019-08-17 13:37:16 -0600

    Delete, Edit buttons hidden

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -57,7 +57,7 @@ router.get("/:id/edit", checkCampgroundOwnership, function(req, res){
 });
 
 //UPDATE
-router.put("/:id", function(req, res){
+router.put("/:id", checkCampgroundOwnership, function(req, res){
     Campground.findByIdAndUpdate(req.params.id, req.body.campground, function(err, updatedCampground){
         if(err){
             res.redirect("/campgrounds");
@@ -68,7 +68,7 @@ router.put("/:id", function(req, res){
 });
 
 //DESTROY
-router.delete("/:id", function(req, res){
+router.delete("/:id", checkCampgroundOwnership, function(req, res){
     Campground.findByIdAndRemove(req.params.id, function(err){
         if(err){
             res.redirect("/campgrounds");
